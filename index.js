f26b05241e903ac9d08a9cb77a20bf26639d1dcb
shubham05116 <88928071+shubham05116@users.noreply.github.com>
2021-09-13 00:30:30 +0530

    players wining style changed

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// var randomnumber1 = Math.floor(Math.random()*6)+1;
-//
-// var  randomimagesource2 = "dice"+randomnumber1+".png";
-//
-// var image1 = document.querySelectorAll("img")[0];
-// image1.setAttribute("src", randomimagesource2);
-// var  randomnumber2 = Math.floor(Math.random()*6)+1;
-// var randomimagesource2 = "dice"+randomnumber2+".png";
-// document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
-// if(randomnumber1>randomnumber2){
-//   document.querySelector("h1").innerHTML="player 1 wins ";
-// }
-// else if (randomnumber2>randomnumber1)
-// {
-//   document.querySelector("h1").innerHTML= " player 2  wins ";
-// }
-// else{
-//
-//   document.querySelector("h1").innerHTML= " Draw";
-// }
+
 
 
 var randomnumber1 = Math.floor( Math.random()*6)+1;
@@ -33,10 +14,10 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src" , randomimagesource2);
 
 if(randomnumber1>randomnumber2){
-  document.querySelector("h1").innerHTML="Player 1 wins ✔";
+  document.querySelector("h1").innerHTML="Player 1 wins ";
 }
 else if (randomnumber2>randomnumber1) {
-  document.querySelector("h1").innerHTML="Player 2 wins ✔";
+  document.querySelector("h1").innerHTML="Player 2 wins ";
 
 }
 else
